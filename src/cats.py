e5780960e45f2b08227cfb33ec5869d59ad989cb
Adrien Guille <adrien.guille@gmail.com>
2015-09-07 13:06:24 +0200

    Testing

--- src/cats.py
+++ src/cats.py
@@ -44,11 +44,11 @@ query_pretty = ""
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
+        if (request.form['username'] == 'adrien' or request.form['username'] == 'michael') and request.form['password'] == 'test':
             session['name'] = 'admin'
             return collection_dashboard_page()
+        else:
+            error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
 
 
